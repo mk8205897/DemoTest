c0390f183eb6d410a19f9c12dcf1fddad3783b1e
mohit <mk8205897@gmail.com>
2020-08-08 15:07:00 +0530

    added searchPage feature page

--- src/main/java/com/qa/pages/Loginpage.java
+++ src/main/java/com/qa/pages/Loginpage.java
@@ -1,5 +1,14 @@
 package com.qa.pages;
 
 public class Loginpage {
+	
+	public void loginPage(){
+		System.out.println("login page");
+	}
+	
+	public void loginSetpage(){
+		System.out.println("loginSet page");
+	}
+
 
 }
